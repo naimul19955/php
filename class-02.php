9278087974795b40c337e95e5d161f9a97300d86
naimul_19955 <freelancernaimul@gmail.com>
2025-09-17 11:48:11 +0600

    Class -02 completed

--- class-02.php
+++ class-02.php
@@ -1,6 +1,31 @@
 <?php
-$a = 10;
-$b = "10";
+// $a = 10;
+// $b = "10";
 
-$output = $a == $b;
-echo $output;
+// $output = $a == $b;
+// echo $output;
+
+// $t = true;
+// $f = false;
+
+// $and = $t && $f;
+// var_dump($and);
+// echo "\n";
+
+// $or = $t || $f;
+// var_dump($or);
+
+
+$a = 5;
+$b = 6;
+
+$c = $a++; //post increment
+echo $c;
+echo "\n";
+$d = ++$b; //pre increment
+echo $d;
+
+
+
+
+
